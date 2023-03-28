833ec0817190e61674754cd1ed1e6e272bd7984e
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-03-28 20:58:21 -0300

    clase02

---
+++ Clase 02/ejercicio01.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import cmath
+
+parser= argparse.ArgumentParser(description='Calcular raices')
+
+parser.add_argument('n', type=int, help='debe ser un número')
+parser.add_argument('-r', '--root', action='store_true', help='es la opción para hallar, también, la raíz negativa')
+
+args = parser.parse_args()
+
+
+r_ans = cmath.sqrt(args.n)
+
+if args.root:
+    rt = os.fork()
+    
+    if rt > 0:
+        print(f'La raíz positiva es: {r_ans}')
+
+    elif rt == 0:
+        print(f'La raíz negativa es: {-r_ans}')
+
+if not args.root:
+    print(f'La raíz positiva es: {r_ans}')
+
+
+
+
+# Realizar un programa que implemente fork junto con el parseo de argumentos. 
+# Deberá realizar relizar un fork si -f aparece entre las opciones al ejecutar el programa.
+# El proceso padre deberá calcular la raiz cuadrada positiva de un numero y el hijo la raiz negativa.
