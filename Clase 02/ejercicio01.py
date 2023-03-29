de3aea4fd9365a5c90fbdc282c46ffabecfe760d
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-03-28 21:21:09 -0300

    pulidita 1.1

--- Clase 02/ejercicio01.py
+++ Clase 02/ejercicio01.py
@@ -27,10 +27,3 @@ if args.root:
 
 if not args.root:
     print(f'La raíz positiva es: {r_ans}')
-
-
-
-
-# Realizar un programa que implemente fork junto con el parseo de argumentos. 
-# Deberá realizar relizar un fork si -f aparece entre las opciones al ejecutar el programa.
-# El proceso padre deberá calcular la raiz cuadrada positiva de un numero y el hijo la raiz negativa.
