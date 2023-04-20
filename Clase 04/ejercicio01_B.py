7e6729bf7faa037f04a95ebabd90545aeb92ac51
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-04-19 22:19:16 -0300

    corregido 100%

--- Clase 04/ejercicio01_B.py
+++ Clase 04/ejercicio01_B.py
@@ -17,44 +17,71 @@ fifo_00 = '/tmp/my_fifo.'
 
 def child_01():
     fifo = open(fifo_00, 'a')
-    mat0 = str(matrix1[0]*matrix2[0]+matrix1[2]*matrix2[1])
-    fifo.write(mat0)
+    mat0 = '00:' + str(matrix1[0]*matrix2[0]+matrix1[2]*matrix2[1])
+    fifo.write(mat0 + '\n')
     fifo.close()
 
 def child_02():
     fifo = open(fifo_00, 'a')
-    mat1 = str(matrix1[1]*matrix2[0]+matrix1[3]*matrix2[1])
-    fifo.write(mat1)
+    mat1 = '01:' +  str(matrix1[1]*matrix2[0]+matrix1[3]*matrix2[1])
+    fifo.write(mat1 + '\n')
     fifo.close()
     
 def child_03():
     fifo = open(fifo_00, 'a')
-    mat2 = str(matrix1[0]*matrix2[2]+matrix1[2]*matrix2[3])
-    fifo.write(mat2)
+    mat2 = '10:' +  str(matrix1[0]*matrix2[2]+matrix1[2]*matrix2[3])
+    fifo.write(mat2 + '\n')
     fifo.close()
 
 def child_04():
     fifo = open(fifo_00, 'a')
-    mat3 = str(matrix1[1]*matrix2[2]+matrix1[3]*matrix2[3])
-    fifo.write(mat3)
+    mat3 = '11:' +  str(matrix1[1]*matrix2[2]+matrix1[3]*matrix2[3])
+    fifo.write(mat3 + '\n')
     fifo.close()
 
 def parent():
     fifo_in = open(fifo_00, 'r')
-    print(fifo_in.read())
+
+    data1 = str(fifo_in.readline().strip())
+    data2 = str(fifo_in.readline().strip())
+    data3 = str(fifo_in.readline().strip())
+    data4 = str(fifo_in.readline().strip())
+
+    data = [data1, data2, data3, data4]
+
+    for d in data:
+        if d[:3] == '00:':
+            m00 = d[3:]
+        elif d[:3] == '01:':
+            m01 = d[3:]
+        elif d[:3] == '10:':
+            m10 = d[3:]
+        elif d[:3] == '11:':
+            m11 = d[3:]
+
+    mat = [[m00,m01],[m10,m11]]
+
+    for m in mat:
+        print(m)
 
 if not os.path.exists(fifo_00):
     os.mkfifo(fifo_00)
 
-pid_list = []
-for i, func in enumerate([child_01, child_02, child_03, child_04]):
-    pid = os.fork()
-    if pid == 0: # proceso hijo
-        func()
-        os._exit(0)
-    else: # proceso padre
-        parent()
 
-# Espera a que todos los procesos hijo terminen su trabajo
-for pid in pid_list:
-    os.waitpid(pid, 0)
+pid1 = os.fork()
+if pid1 == 0:
+    parent()
+
+    pid2 = os.fork()
+    if pid2 == 0:
+        child_02()
+    else:
+        pid3 = os.fork()
+        if pid3 == 0:
+            child_03()
+        else:
+            pid4 = os.fork()
+            if pid4 == 0:
+                child_04()
+else:
+    child_01()
