3f8d15a324e55e464e7ec9976e7441f62dcd87d9
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2023-05-04 23:33:08 -0300

    idea - revisar

--- Clase 05/noblock.py
+++ Clase 05/noblock.py
@@ -5,8 +5,9 @@ https://github.com/satwikkansal/python_blockchain_app
 
 from hashlib import sha256
 import json
+import os, signal
 
-
+fifo_hash = '/tmp/my_fifo.'
 
 class NoBlock:
     def __init__(self, seed, nonce=0):
@@ -27,24 +28,45 @@ def proof_of_work(block):
     Function that tries different values of nonce to get a hash
     that satisfies our difficulty criteria.
     """
-    difficulty = 2
+    difficulty = 6
 
     computed_hash = block.compute_hash()
     while not computed_hash.startswith('0' * difficulty):
         block.nonce += 1
         computed_hash = block.compute_hash()
 
-    return computed_hash
+    return computed_hash, block.nonce
     
- 
-b = NoBlock(seed='La semilla que quiera', nonce=0)
-h = b.compute_hash()
-new_hash = proof_of_work(b)
-print(new_hash)
+def handle_signal(signum, frame):
+    print("Señal recibida.")
+
+    with open(fifo_hash, 'r') as fifo:
+        nonce = fifo.read()
+
+        print(nonce)
+        exit(0)
 
 
-"""
-os.ppid() # obtiene el pid del padre
-os.kill(<señal>, <pid>)
-"""
+for i in range(2):
+    pid = os.fork()
+    if pid == 0:
+        print(f'{os.getpid()} hijo DE {os.getppid()}')
+        record = os.getpid()
 
+        b = NoBlock(seed='La semilla que quiera', nonce=0)
+        h = b.compute_hash()
+        new_hash = proof_of_work(b)
+        nonce = new_hash[1]
+        nonce_str = str(nonce)
+
+        os.kill(os.getppid(), signal.SIGUSR1)
+
+        fifo = open(fifo_hash, 'w')
+        fifo.write(f'El nonce encontrado es: {nonce_str}\nLo encontro: {record}')
+        fifo.close()
+
+        exit()
+        
+
+signal.signal(signal.SIGUSR1, handle_signal)
+os.wait()
